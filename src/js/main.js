7f442171580855abfe41a1dba6571890aad6bc4a
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2023-09-14 00:03:58 +0600

    header and promo mobile + destkop

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,36 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('#menu'),
+        openMenuBtn = document.querySelector('#open-menu'),
+        closeMenuBtn = document.querySelector('#close-menu'),
+        overylay = document.querySelector('.overlay'),
+        accordeonBtn = document.querySelector('#accordeon-btn'),
+        accordeonDropdown = document.querySelector('#accordeon-dropdown');
 
+    accordeonBtn.addEventListener('click', function (e) {
+        console.log(accordeonDropdown.hasAttribute('style'))
+        if (accordeonDropdown.hasAttribute('style')) {
+            accordeonDropdown.removeAttribute('style');
+            e.target.classList.remove('active');
+        } else {
+            accordeonDropdown.style.maxHeight = accordeonDropdown.scrollHeight + 40 + "px";
+            accordeonDropdown.style.paddingTop = 30 + "px";
+            e.target.classList.add('active');
+        }
+
+    })
+
+    function openMenu(e) {
+        overylay.classList.add('active');
+        menu.classList.add('active');
+    };
+
+    function closeMenu(e) {
+        overylay.classList.remove('active');
+        menu.classList.remove('active');
+    };
+
+    openMenuBtn.addEventListener('click', openMenu);
+    closeMenuBtn.addEventListener('click', closeMenu);
+    overylay.addEventListener('click', closeMenu);
 })
